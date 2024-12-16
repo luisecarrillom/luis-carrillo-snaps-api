0d1331d3261a2201ffd9e26e7fb99a1ffcb1d5fd
luisecarrillom <luisecarrillom@gmail.com>
2024-12-16 15:10:08 -0700

    Update'

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use(cors());
+app.use(cors({ origin: "*" })); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/photos", express.static(path.join(__dirname, "public/images")));
 
@@ -19,10 +17,17 @@ app.use("/photos", express.static(path.join(__dirname, "public/images")));
 app.use("/api/photos", photosRoutes);
 app.use("/api/tags", tagsRoutes);
 
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use((req, res) => {
+  res.status(404).send({ message: "Route not found" });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
